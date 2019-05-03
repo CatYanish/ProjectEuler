8037bbb7fa893b03f5e6e69d4d75966ca95f66c3
Catherine Yanish <catherine.yanish@sportshubtech.com>
2019-05-03 12:23:37 -0500

    solutions

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+
+// #1
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+function findAllMultiplesBelowGivenNumber (factor1, factor2, givenNumber) {
+  let sum = 0;
+
+  for (let i = 0; i < givenNumber; i++ ) {  
+    if ((i % factor1 === 0) || (i % factor2 === 0 )) {
+      sum = sum += i;
+    }
+  }
+
+return sum
+} 
+
+const problemOneSum = findAllMultiplesBelowGivenNumber(3, 5, 1000);
+console.log(problemOneSum);
+
+
+
+
+
+// #2
 let numbers = [1, 2 ];
 var a = numbers[numbers.length - 2]; 
 var b = numbers[numbers.length - 1];
@@ -12,5 +39,7 @@ while ( (a + b) < 4000000 ) {
 if ( (a + b) > 4000000) {
   var evens = numbers.filter(number => number % 2 === 0 )
   const reducer = (acc, currentValue) => acc + currentValue;
-  evens.reduce(reducer);
+  const evensSum = evens.reduce(reducer);
+  console.log(evensSum);
+  
 }
