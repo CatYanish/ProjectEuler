31331d0869b9da2665334d80c13e36ebebd75c9d
Cat Yanish <catyanish@gmail.com>
2019-05-23 17:55:33 -0500

    found solution online for problem 3

--- index.js
+++ index.js
@@ -8,14 +8,14 @@
 // function findAllMultiplesBelowGivenNumber (factor1, factor2, givenNumber) {
 //   let sum = 0;
 
-//   for (let i = 0; i < givenNumber; i++ ) {  
+//   for (let i = 0; i < givenNumber; i++ ) {
 //     if ((i % factor1 === 0) || (i % factor2 === 0 )) {
 //       sum = sum += i;
 //     }
 //   }
 
 // return sum
-// } 
+// }
 
 // const problemOneSum = findAllMultiplesBelowGivenNumber(3, 5, 1000);
 // console.log(problemOneSum);
@@ -26,12 +26,12 @@
 
 // // #2
 // let numbers = [1, 2 ];
-// var a = numbers[numbers.length - 2]; 
+// var a = numbers[numbers.length - 2];
 // var b = numbers[numbers.length - 1];
 
 // while ( (a + b) < 4000000 ) {
 //   numbers.push(a + b);
-//   a = numbers[numbers.length - 2]; 
+//   a = numbers[numbers.length - 2];
 //   b = numbers[numbers.length - 1];
 // }
 
@@ -41,7 +41,7 @@
 //   const reducer = (acc, currentValue) => acc + currentValue;
 //   const evensSum = evens.reduce(reducer);
 //   console.log(evensSum);
-  
+
 // }
 
 
@@ -49,51 +49,80 @@
 
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
-// ideas: generate a list of prime numbers. See which ones are factors of given number. return the biggest one. 
-// ideas: find all factors of given number. If number is prime and bigger than starting number, return it. 
+// ideas: generate a list of prime numbers. See which ones are factors of given number. return the biggest one.
+// ideas: find all factors of given number. If number is prime and bigger than starting number, return it.
+
+//
+// function isPrime(num) {
+//   if (num < 0 || num === 1 || ((typeof num !== "number") && Math.floor(num) !== num)) {
+//     return false;
+//   } else if (num === 2) {
+//     return true;
+//   } else if ( num % 2 !== 0 ) {
+//     let isPrime = true;
+//     for (let index = 1; index < 10; index++) {
+//       if (num % index === 0 && (index !== 1 && index !== num)) {
+//         isPrime = false;
+//       }
+//     }
+//     return isPrime;
+//   } else {
+//     return false;
+//   }
+// }
+//
+// // const isNumPrime = isPrime(9973);
+// // console.log(isNumPrime);
+// // const isNotPrime = isPrime(888888)
+// // console.log(isNotPrime);
+//
+//
+// // function getLargestPrimeFactorOfGivenNumber (givenNumber) {
+// //   let i = givenNumber;
+// //   let largestPrime;
+// //   do {
+// //     i = i - 1;
+// //     if ((givenNumber % i === 0) && (i % 2 !== 0) ) {
+// //       console.log('divides evenly and is odd number: i:', i)
+// //       // const isPrimeFactor = isPrime(i)
+// //       // if (isPrimeFactor === true) {
+// //       //   largestPrime = i;
+// //       // }
+// //     }
+// //   } while (i > (givenNumber / 2) && largestPrime === undefined);
+// //   return largestPrime;
+// // }
+//
+//
+// const createArrayOfOddFactors = (givenNumber) => {
+//   let oddFactors = []
+//     for (var i = (givenNumber-1); i > 2; i--) {
+//       console.log(i);
+//       if (givenNumber % i === 0) {
+//         oddFactors.push(i)
+//       }
+//   }
+//   return oddFactors;
+// }
+//
+// console.log(createArrayOfOddFactors(600851475143));
+
+// const primeNum = getLargestPrimeFactorOfGivenNumber(600851475143);
+//
+// console.log({primeNum})
 
 
-function isPrime(num) {
-  if (num < 0 || num === 1 || ((typeof num !== "number") && Math.floor(num) !== num)) {
-    return false;
-  } else if (num === 2) {
-    return true;
-  } else if ( num % 2 !== 0 ) {
-    let isPrime = true; 
-    for (let index = 1; index < 10; index++) {
-      if (num % index === 0 && (index !== 1 && index !== num)) {
-        isPrime = false;
-      }
-    }
-    return isPrime;
-  } else {
-    return false;
-  }
+
+var originalTarget = 600851475143;
+var target = originalTarget;
+var i = 2;
+while(i<target) {
+	while(target%i === 0) {
+		(function(newtarget) {
+			console.log(target + " can be divided by " + i + " which gives us " + newtarget);
+			target = newtarget;
+		})(target / i)
+	}
+	i++;
 }
-
-// const isNumPrime = isPrime(9973);
-// console.log(isNumPrime);
-// const isNotPrime = isPrime(888888)
-// console.log(isNotPrime);
-
-
-function getLargestPrimeFactorOfGivenNumber (givenNumber) {
-  let i = givenNumber;
-  let largestPrime;
-  do {
-    i = i - 1;
-    if ((givenNumber % i === 0) && (i % 2 !== 0) ) {
-      console.log('divides evenly and is odd number: i:', i)
-      // const isPrimeFactor = isPrime(i)
-      // if (isPrimeFactor === true) {
-      //   largestPrime = i;        
-      // } 
-    }
-  } while (i > 0 && largestPrime === undefined);
-  return largestPrime;
-}
-
-
-const primeNum = getLargestPrimeFactorOfGivenNumber(600851475143);
-
-console.log({primeNum})
+console.log("it seems like " + target + " is the biggest prime factor for " + originalTarget);
