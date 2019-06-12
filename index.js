5090163bda7778f1b6dd00636d96f02edc4aa76e
Cat Yanish <catyanish@gmail.com>
2019-06-12 14:47:53 -0500

    mamas still got it

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 
-// // #1
+// #1
 
-// // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-// // Find the sum of all the multiples of 3 or 5 below 1000.
+// Find the sum of all the multiples of 3 or 5 below 1000.
 
-// function findAllMultiplesBelowGivenNumber (factor1, factor2, givenNumber) {
+// function findSumOfAllMultiplesBelowGivenNumber (factor1, factor2, givenNumber) {
 //   let sum = 0;
-
+//
 //   for (let i = 0; i < givenNumber; i++ ) {
 //     if ((i % factor1 === 0) || (i % factor2 === 0 )) {
 //       sum = sum += i;
 //     }
 //   }
-
+//
 // return sum
 // }
-
-// const problemOneSum = findAllMultiplesBelowGivenNumber(3, 5, 1000);
+//
+// const problemOneSum = findSumOfAllMultiplesBelowGivenNumber(3, 5, 1000);
 // console.log(problemOneSum);
 
 
@@ -28,20 +28,20 @@
 // let numbers = [1, 2 ];
 // var a = numbers[numbers.length - 2];
 // var b = numbers[numbers.length - 1];
-
+//
 // while ( (a + b) < 4000000 ) {
 //   numbers.push(a + b);
 //   a = numbers[numbers.length - 2];
 //   b = numbers[numbers.length - 1];
 // }
-
-
+//
+//
 // if ( (a + b) > 4000000) {
 //   var evens = numbers.filter(number => number % 2 === 0 )
 //   const reducer = (acc, currentValue) => acc + currentValue;
 //   const evensSum = evens.reduce(reducer);
 //   console.log(evensSum);
-
+//
 // }
 
 
@@ -106,7 +106,7 @@
 // 	i++;
 // }
 // console.log("it seems like " + target + " is the biggest prime factor for " + originalTarget);
-
+//
 
 
 // integer to roman numerals
@@ -194,13 +194,46 @@
 // reverseAString('reversing strings in my free time');
 
 
-const reverseStringTheLongWay = (string) => {
-	const arrayOfChar = string.split('');
+// const reverseStringTheLongWay = (string) => {
+// 	const arrayOfChar = string.split('');
+// 	let reversed = "";
+// 	for (let i = 0; i < arrayOfChar.length; i++) {
+// 		reversed = arrayOfChar[i] + reversed;
+// 	}
+// 	return reversed;
+// }
+//
+// console.log(reverseStringTheLongWay('cat'));
+
+
+// let longString = "abcabcddbaaaaa";
+//
+// const countUpCharacters = function (str) {
+// 	const array = str.split('');
+// 	const countedChars = array.reduce(function(acc, item) {
+// 		if(acc[item] === undefined) {
+// 			acc[item] = 1;
+// 		} else {
+// 			acc[item] += 1;
+// 		}
+// 		return acc;
+// 	}, {})
+// 	return countedChars;
+// }
+//
+//
+// console.log(countUpCharacters(longString));
+
+
+const string = 'bat';
+
+const reverseStringWithoutBuiltInMethods = function (str) {
+	let array = str.split('');
 	let reversed = "";
-	for (let i = 0; i < arrayOfChar.length; i++) {
-		reversed = arrayOfChar[i] + reversed;
+	for (var i = 0; i < array.length; i++) {
+		reversed = array[i] + reversed;
 	}
 	return reversed;
 }
 
-console.log(reverseStringTheLongWay('cat'));
+console.log(reverseStringWithoutBuiltInMethods(string));
